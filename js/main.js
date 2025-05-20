091dc8d7761a673437321d96bf800eafd5aac89d
Vincent-tt-cyber <evgeniy161102@gmail.com>
2025-05-20 12:09:09 +0300

    Создн дизайн с кастомным чекбоксом

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("test");
+let input = document.querySelector(".header__input");
+let addBtn = document.querySelector(".header__add-btn");
+
+let todoItems = document.querySelectorAll(".todo-item");
+
+for (let todoItem of todoItems) {
+  todoItem.addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
