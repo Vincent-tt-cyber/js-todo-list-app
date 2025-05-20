b9abf2c96bcae0f3ffb568419c426e9a7c7d635f
Vincent-tt-cyber <evgeniy161102@gmail.com>
2025-05-20 12:25:39 +0300

    Созданы функции для создания задачи

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 let input = document.querySelector(".header__input");
 let addBtn = document.querySelector(".header__add-btn");
 
+function createRandomTodoID(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// Создание задачи
+function createNewTodo(title) {
+  return {
+    id: createRandomTodoID(1, 100),
+    title,
+  };
+}
+
+addBtn.addEventListener("click", function () {
+  // Создание задачи
+  let newTask = createNewTodo(input.value);
+});
+
 let todoItems = document.querySelectorAll(".todo-item");
 
 for (let todoItem of todoItems) {
